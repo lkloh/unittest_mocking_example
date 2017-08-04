6021a2cd7ec29c373d3f81cab4a6af8a45773740
LK Loh <lkloh2410@gmail.com>
2017-08-04 10:59:09 -0700

    How to do things after a decorator

--- decorator.py
+++ decorator.py
@@ -1,18 +1,23 @@
 
-def print_function_return_value_decorator(should_print):
+# https://stackoverflow.com/questions/10176226/how-to-pass-extra-arguments-to-python-decorator
+# https://stackoverflow.com/questions/14703310/how-can-i-get-a-python-decorator-to-run-after-the-decorated-function-has-complet
+
+def print_function_return_value_decorator(decorator_args):
 	def actual_decorator(func):
 		def wrapper(*args, **kwargs):
-			print(*args)
-			if should_print:
-				print("This is called third")
-			return func(*args, **kwargs)
-		print("This is called second")
+			if decorator_args:
+				print("Decorator args are %s" % str(decorator_args))
+				print(*args)
+			invoke_unit_test = func(*args, **kwargs)
+			print('After the get_name was called!')
+			return invoke_unit_test
 		return wrapper
-	print("This is called first")
+	print("Before get_name")
 	return actual_decorator
 
-@print_function_return_value_decorator(True)
+@print_function_return_value_decorator("Whizz Bang")
 def get_name(name):
+	print("You just did something inside get_name!")
 	return name
 
 get_name("John Doe")
