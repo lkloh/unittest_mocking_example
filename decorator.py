d3bbc1e16a51d3c750b61305f2b226216c54550b
LK Loh <lkloh2410@gmail.com>
2017-08-04 10:26:52 -0700

    How decorators work

--- decorator.py
+++ decorator.py
@@ -1,11 +1,16 @@
 
-def my_decorator(my_func):
-	def wrapper(name):
-		return my_func(name)
-	return wrapper
+def print_function_return_value_decorator(should_print):
+	def actual_decorator(func):
+		def wrapper(*args, **kwargs):
+			if should_print:
+				print("This is called second")
+			return func(*args, **kwargs)
+		return wrapper
+	print("This is called first")
+	return actual_decorator
 
-@my_decorator
+@print_function_return_value_decorator(True)
 def get_name(name):
-   return name
+	return name
 
-print(get_name("John Doe"))
+get_name("John Doe")
