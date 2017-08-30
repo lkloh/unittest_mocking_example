679a435303e1ea932da63736a0fa6008980c3044
LK Loh <lkloh2410@gmail.com>
2017-08-30 11:27:58 -0700

    Factor assert stmt

--- decorators/factoring_decorators.py
+++ decorators/factoring_decorators.py
@@ -3,7 +3,7 @@ def decorator(upper_bound, assert_stmt):
 
 	def outer_wrapper(func):
 
-		def inner_wrapper(func_arg, upper_bound):
+		def inner_wrapper(func_arg):
 			print('before')
 			val = func(func_arg)
 			assert_stmt(val, upper_bound)
@@ -13,17 +13,35 @@ def decorator(upper_bound, assert_stmt):
 
 	return outer_wrapper
 
-def assert_less_than(val, n):
-	assert val <= n
+def decorator_less_than(upper_bound):
 
-def assert_equal(val, n):
-	assert val == n
+	def assert_less_than(val, n):
+		assert val <= n
 
-@decorator(56, assert_less_than)
-def func(arg):
+	return decorator(upper_bound, assert_less_than)
+
+def decorator_equal(upper_bound):
+
+	def assert_equal(val, n):
+		assert val == n
+
+	return decorator(upper_bound, assert_equal)
+
+# testing
+
+@decorator_less_than(100)
+def func_less_than(arg):
 	return arg * 7
 
-func(8, assert_less_than)
+func_less_than(8)
+
+@decorator_equal(56)
+def func_equal(arg):
+	return arg * 7
+
+func_equal(8)
+
+
 
 
 
